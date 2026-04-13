dbcc828e88b62aa00b8093b8d3391d8b62db0240
Diego Rafael Mieth <dgmieth@gmail.com>
2026-04-12 22:08:29 -0400

    adding tests - init

---
+++ Findit/FinditTests/UnitTests/DateFormatterTests.swift
@@ -0,0 +1,43 @@
+//
+//  DateFormatterTests.swift
+//  FinditTests
+//
+//  Created by Diego Mieth on 2026-04-12.
+//
+
+import XCTest
+
+final class DateFormatterTests: XCTestCase {
+    
+    private func getDate() -> Date? {
+        var dateComponents = DateComponents()
+        dateComponents.year = 2026
+        dateComponents.month = 04
+        dateComponents.day = 12
+        dateComponents.hour = 22
+        dateComponents.minute = 02
+        dateComponents.second = 00
+        
+        return Calendar.current.date(from: dateComponents)
+    }
+
+    func testDateFormatterApiDateFormat() throws {
+        let testDate = getDate()
+        XCTAssertNotNil(testDate)
+        
+        let formatter = DateFormatter.formatter(for: .apiDateFormat)
+        let formattedDateString = formatter.string(from: testDate!)
+        
+        XCTAssertEqual(formattedDateString, "2026-04-12 22:02:00")
+    }
+    
+    func testDateFormatterApiQueryFormat() throws {
+        let testDate = getDate()
+        XCTAssertNotNil(testDate)
+        
+        let formatter = DateFormatter.formatter(for: .apiQueryFormat)
+        let formattedDateString = formatter.string(from: testDate!)
+        
+        XCTAssertEqual(formattedDateString, "2026-04-12")
+    }
+}
---
+++ Findit/FinditTests/UnitTests/FinditTests.swift
@@ -0,0 +1,69 @@
+//
+//  FinditTests.swift
+//  FinditTests
+//
+//  Created by Diego Mieth on 2026-04-12.
+//
+
+import XCTest
+@testable import Findit
+
+struct MockedEndpoint: EndpointProtocol {
+    var path: String = "/path"
+    var paramOne: String?
+    var paramTwo: String?
+    
+    init(paramOne: String? = nil, paramTwo: String? = nil) {
+        self.paramOne = paramOne
+        self.paramTwo = paramTwo
+    }
+    
+    var queryParams: Findit.QueryParams? {
+        switch (self.paramOne, self.paramTwo) {
+        case (.some(let one), .some(let two)):
+            return [
+                "one": one,
+                "two": two,
+            ]
+        case (.some(let one), _):
+            return [
+                "one": one,
+            ]
+        case (_, .some(let two)):
+            return [
+                "two": two,
+            ]
+        default: return nil
+        }
+    }
+}
+
+final class EndpointProtocolTests: XCTestCase {
+    func testEndpointNoParams() throws {
+        let _endpoint = MockedEndpoint()
+        let request = _endpoint.generateRequest(for: "https:example.com")
+        XCTAssertNotNil(request?.url?.absoluteString)
+        XCTAssertEqual(request!.url!.absoluteString, "https:example.com/path")
+    }
+    
+    func testEndpointParamOne() throws {
+        let _endpoint = MockedEndpoint(paramOne: "One")
+        let request = _endpoint.generateRequest(for: "https:example.com")
+        XCTAssertNotNil(request?.url?.absoluteString)
+        XCTAssertEqual(request!.url!.absoluteString, "https:example.com/path?one=One")
+    }
+    
+    func testEndpointParamTwo() throws {
+        let _endpoint = MockedEndpoint(paramTwo: "Two")
+        let request = _endpoint.generateRequest(for: "https:example.com")
+        XCTAssertNotNil(request?.url?.absoluteString)
+        XCTAssertEqual(request!.url!.absoluteString, "https:example.com/path?two=Two")
+    }
+    
+    func testEndpointParams() throws {
+        let _endpoint = MockedEndpoint(paramOne: "One", paramTwo: "Two")
+        let request = _endpoint.generateRequest(for: "https:example.com")
+        XCTAssertNotNil(request?.url?.absoluteString)
+        XCTAssertEqual(request!.url!.absoluteString, "https:example.com/path?one=One&two=Two")
+    }
+}
